2d006662ce1576f46e432bdd6faac81ab6af33c6
yuliakordunova97 <kordunovayulia@gmail.com>
2022-03-16 10:09:17 +0200

    added files

---
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -0,0 +1,14 @@
+package com.epam.rd.autotasks.godutch;
+
+import java.util.Scanner;
+
+public class GoDutch {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int bill = scanner.nextInt();
+        int people = scanner.nextInt();
+
+
+    }
+}
