27a604cf28e6ea08015300b168f5d2edd7ce02db
YuraLen <92933581+YuraLen@users.noreply.github.com>
2023-03-13 08:55:10 +0200

    done

--- src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -9,6 +9,15 @@ public class GoDutch {
         int bill = scanner.nextInt();
         int people = scanner.nextInt();
 
-
+        if(bill >= 0){
+            int tips = bill / 100 * 10;
+            if(people > 0){
+                System.out.println((bill + tips) / people);
+            } else {
+                System.out.println("Number of friends cannot be negative or zero");
+            }
+        } else {
+            System.out.println("Bill total amount cannot be negative");
+        }
     }
 }
